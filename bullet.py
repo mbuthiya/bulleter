3566ed572cdda9e5473b11ab893b3c904e7cab4b
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-07-09 06:09:02 +0300

    Copy from clipboard alter and paste to clipboard Copied text from my computer clipboard Added a star before each new line joined back the string pasted the alterd text back to clip board

--- bullet.py
+++ bullet.py
@@ -1,3 +1,15 @@
-#! /usr/bin/python3.6
-# A program that takes text from a clipboard and puts bullet points on each line then pastes the
+#! usr/bin/python3.6
+# A simple program that takes the items from the clipboard and adds a star infron of every new line and return it to the clipboard
+
 import pyperclip
+
+text = pyperclip.paste()
+
+lines = text.split("\n")
+
+for i in range(len(lines)):
+    lines[i] = '* '+lines[i]
+
+text = "\n".join(lines)
+
+pyperclip.copy(text)
